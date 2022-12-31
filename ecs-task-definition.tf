e7f5475a1b70737d4153ce1df7c98c2540799b5e
emirmandal <emir.enetel@gmail.com>
2022-12-31 01:52:43 +0100

    Create ECS services and EC2 database

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -27,9 +27,90 @@ resource "aws_ecs_task_definition" "client" {
         {
           name  = "MESSAGE"
           value = "Hello from the client!"
+        },
+        {
+          name  = "UPSTREAM_URIS"
+          value = "http://${aws_lb.fruits_alb.dns_name},http://${aws_lb.vegetables_alb.dns_name}"
         }
       ]
     }
   ])
+}
 
+resource "aws_ecs_task_definition" "fruits" {
+  family                   = "${var.default_tags.project}-fruits"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name      = "fruits"
+      image     = "nicholasjackson/fake-service:v0.23.1"
+      cpu       = 0
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 9090
+          hostPort      = 9090
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "NAME"
+          value = "fruits"
+        },
+        {
+          name  = "MESSAGE"
+          value = "Hello from the fruits client!"
+        },
+        {
+          name  = "UPSTREAM_URIS"
+          value = "http://${var.database_private_ip}:27017"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "vegetables" {
+  family                   = "${var.default_tags.project}-vegetables"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name      = "vegetables"
+      image     = "nicholasjackson/fake-service:v0.23.1"
+      cpu       = 0
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 9090
+          hostPort      = 9090
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "NAME"
+          value = "vegetables"
+        },
+        {
+          name  = "MESSAGE"
+          value = "Hello from the vegetables client!"
+        },
+        {
+          name  = "UPSTREAM_URIS"
+          value = "http://${var.database_private_ip}:27017"
+        }
+      ]
+    }
+  ])
 }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ec2_key_pair        = "MyKeyPair3"
+database_private_ip = "10.255.2.253"
